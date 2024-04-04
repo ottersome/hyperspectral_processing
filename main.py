a37640e050ab6acaffcf81a9048e3e8a551f8373
Luis Garcia <luisgarcia26284@gmail.com>
2024-04-04 19:28:42 +0800

    Added: Preprocessing into cache

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
-import logging
+import enum
 import os
 from argparse import ArgumentParser
 from collections import namedtuple
+from math import pi
 from typing import Any, List, Tuple, Union
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
-from torch.autograd import set_detect_anomaly
+from torch.autograd import set_detect_anomaly  # type: ignore
 from tqdm import tqdm
 
+from hyptraining.datap.data import preprocess_data
+from hyptraining.utils.utils import create_logger, unused
+
+
+# Create Enum for filetype
+class FileType(enum.Enum):
+    CSV = "csv"
+    PARQUET = "parquet"
+    ENVI = "envi"
+
+
+STR_TO_FILE = {k.value: k for k in FileType}
+
+from image_processing import (
+    Point,
+    chords_to_circle,
+    find_distinctive_feature_coords,
+    get_final_image,
+)
 from model import Model
 
 set_detect_anomaly(True)
 
 DataSet = List[List[Any]]
 
-
-def create_logger():
-    logger = logging.getLogger("MAIN")
-    logger.setLevel(logging.DEBUG)
-    cwd = os.getcwd()
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    fh = logging.FileHandler(os.path.join(cwd, "./logs"), "w")
-    fh.setFormatter(formatter)
-    sh = logging.StreamHandler()
-    sh.setFormatter(formatter)
-    fh.setLevel(logging.DEBUG)
-    sh.setLevel(logging.INFO)
-    logger.addHandler(fh)
-    logger.addHandler(sh)
-
-    return logger
+# For debugging later
+logger = create_logger(os.path.abspath(__file__))
 
 
 def arguments():
     """
     Setup Arguments
     """
+    # Parameters for data retrieval
     ap = ArgumentParser()
     ap.add_argument(
-        "--thick_loc",
-        default="./data/thickness.parquet",
-        help="Dataset A (Hyperspectra) location (Assuming CSV)",
+        "--rawdata_path",
+        default="./data/raw/",
+        help="Directory where objective and hyperspectral data will be stored",
     )
     ap.add_argument(
-        "--hyp_loc",
-        default="./data/hyperspec.parquet",
-        help="Dataset B Location (Assuming CSV)",
+        "--cache_path",
+        default="./data/cache/",
+        help="Where to store post-processed data.",
     )
-    ap.add_argument("--input_size", default=122, help="Size of model's input")
+
+    ap.add_argument("--image_channels", default=122, help="Size of model's input")
     ap.add_argument("--image_height", default=1280, help="Hyperspectral image height.")
     ap.add_argument("--image_width", default=1024, help="Hyperspectral image width")
+    ap.add_argument(
+        "--feature_angle",
+        default=pi / 2,
+        help="How we want the feature to be oriented on all data points.",
+    )
+    ap.add_argument(
+        "--template_location", default=42, help="Location for detecting feature."
+    )
+
+    # Parmeters for training
     ap.add_argument("--epochs", default=10, type=int, help="Training Epochs")
     ap.add_argument("--batch_size", default=32, help="Batch Size")
     ap.add_argument("--random_seed", default=42, help="Seed for psudo-randomness")
@@ -64,16 +81,7 @@ def arguments():
     return ap.parse_args()
 
 
-def thickness_to_hyper_coords(
-    x, y, img_height, img_width
-) -> Union[tuple[int, int], None]:
-    """
-    function you may fill with a mapping of:
-        (u,v) coordinates in wafer space to (x,y) coordinates in pixel space
-    """
-    return (x * (483 / 150) + 515, y * (483 / 150) + 518)
-
-
+@unused
 def build_learning_ds(
     ds_thickness: pd.DataFrame,
     ds_hyper: pd.DataFrame,
@@ -87,9 +95,9 @@ def build_learning_ds(
     """
 
     for row_idx, row in ds_thickness.iterrows():
-        x, y = thickness_to_hyper_coords(
-            row["X"], row["Y"], hyp_img_height, hyp_img_width
-        )
+        # x, y = thickness_to_hyper_coords(
+        #     row["X"], row["Y"], hyp_img_height, hyp_img_width
+        # )
         # Calculate indices for flat list
         ds_hyper_idx = x * hyp_img_width + y
         hyperspec_vec = ds_hyper.iloc[ds_hyper_idx, 2:]
@@ -102,56 +110,87 @@ def build_learning_ds(
     return ds
 
 
-def infill_missing_point(row: List[Any]):
-    # TODO: Fill in with the right typ of content
+def infill_missing_point(row: pd.Series):
+    # TODO: Fill in with the right type of content
     pass
 
-def process_missing_points(ds: DataSet) -> DataSet:
-    # This might require something like compressed sensing, matrix reduction,etc
-    for row_idx, row in df.iterrows():
-        # If we have a missing point, we need to fill it in
-        if row.isnull().any():
-            pass
-        else:
-            infill_missing_point(row)
-    return ds # CHECK: Ensure this is correct
+
+def N_channels_to_one(image: np.ndarray) -> np.ndarray:
+    # TODO: You can apply whatever technique you want here to change it from n channels to 1
+    # Ill just to mean for now
+    return np.mean(image, axis=2)
+    # You could also just select one of the channels in question
 
 
-def find_orientating_feature(template)
+# def load_cached_data(cache_path: str) -> List[List[Tuple]]:
+def load_cached_data(cache_path: str) -> pd.DataFrame:
+    # Ensure .cache path exists
+    assert os.path.exists(cache_path) and os.path.isdir(
+        cache_path
+    ), "Cache path does not exist. This error should not happen"
+
+    # Iterate through the cache path
+    load_bar = tqdm(
+        total=len([f for f in os.listdir(cache_path) if f.endswith(".parquet")]),
+        desc="Loading Cache",
+    )
+    samples: List[List] = []
+    for file in os.listdir(cache_path):
+        # Check if the file is an .npy
+        if ".parquet" in file:
+            df = pd.read_parquet(file)
+            for _, row in df.iterrows():
+                samples.append(row.tolist())
+        load_bar.update(1)
+    logger.info(f"Obtained a total of {len(samples)} files")
+
+    frame = pd.DataFrame(samples).sample(frac=1).reset_index(drop=True)
+    return frame
+
 
 if __name__ == "__main__":
     args = arguments()
     # Set random seeds
     torch.manual_seed(args.random_seed)
 
-    logger = create_logger()  # For storing information for debugging
-
     # Parquet format is a lot lighter than csv
-    logger.info("Loading Thickness Dataset A ")
-    ds_a = pd.read_parquet(args.thick_loc)  # Dataset A
-    ds_a = ds_a.sample(frac=1).reset_index(drop=True)  # Shuffle (best for learning)
+    logger.info(f"Preprocesing data at {args.rawdata_path}")
 
-    logger.info("Loading Hypersectral Dataset B")
-    ds_b = pd.read_parquet(args.hyp_loc)  # Dataset B
+    # Look for new data and convert to data that is easier to process
+    preprocess_data(
+        args.rawdata_path,
+        args.cache_path,
+        args.template_location,
+        args.image_width,  # TODO: perhaps read this from the files themselves
+        args.image_height,
+        args.image_channels,
+        args.feature_angle,
+    )
 
+    logger.info("Finished Preprocessing")
+
+    # Load all cached files
+    dataset = load_cached_data(args.cache_path)
+
+    logger.info("Dataset loaded")
+    # Sample Uniformly
+    exit()
     # Create model-related objects
-    model = Model(args.input_size, 1)
+    model = Model(args.channels, 1)
     logger.info("Model Structure looks like:")
     logger.info(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
     criterium = torch.nn.MSELoss()
-    # scheduler  = ... (if necessary)
+    # TODO: scheduler  = ... (if necessary)
 
     # Build X -> Y Dataset
-    dataset = build_learning_ds(ds_a, ds_b, args.image_height, args.image_width)
+    # dataset = build_learning_ds(ds_a, ds_b, args.image_height, args.image_width)
     # Create Train-Validation split
+
     ds_train = dataset[: int(len(dataset) * args.train_val_split[0])]
     ds_val = dataset[int(len(dataset) * args.train_val_split[0]) :]
 
-    #TODO: Add support for missing points using compressed sensing
-    ds_train = process_missing_points(ds_train)
-    ds_val = process_missing_points(ds_val)
-
+    # TODO: Add support for missing points using compressed sensing
 
     # Train Loop
     num_batches = len(ds_train) // args.batch_size  # Throwing remainder
@@ -192,7 +231,6 @@ if __name__ == "__main__":
             val_losses.append(val_loss.item())
         model.train()
 
-
     # Show Train-Test Performance
     # logger.info(f"Train Losses: {train_losses}")
     # logger.info(f"Val Losses: {val_losses}")
@@ -200,7 +238,7 @@ if __name__ == "__main__":
     plt.plot(train_losses, label="Train Losses")
     plt.plot(val_losses, label="Val Losses")
     plt.legend()
-    plt.title("MSE Loss vs Epochs")
+    plt.title("RMSE Loss vs Epochs")
     plt.ylabel("MSE Loss")
     plt.xlabel("Training Epochs")
     plt.show()
