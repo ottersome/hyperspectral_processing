acf7f66c3d3dc56256dd790a3799b9502b617f21
Luis Garcia <luisgarcia26284@gmail.com>
2024-04-30 22:26:05 +0800

    Changed: model made into more traditional shape

--- model.py
+++ model.py
@@ -8,12 +8,24 @@ class Model(nn.Module):
     def __init__(self, input_size=120, output_size=1):
         super(Model, self).__init__()
 
-        layers = []
-        for i in range(self.LAYERS):
-            layers.append(nn.Linear(input_size // (2**i), input_size // (2 ** (i + 1))))
-            layers.append(nn.ReLU())
+        layers = [
+            nn.Linear(input_size,input_size*2),
+            nn.ReLU(),
+            nn.Linear(input_size*2,input_size*4),
+            nn.ReLU(),
+            nn.Linear(input_size*4,input_size*2),
+            nn.ReLU(),
+            nn.Linear(input_size*2,input_size*1),
+            nn.ReLU(),
+            nn.Linear(input_size,1)
+            
+            ]
+        # for i in range(self.LAYERS):
+        #     layers.append(nn.Linear(input_size // (2**i), input_size // (2 ** (i + 1))))
+        #     layers.append(nn.ReLU())
+        
 
-        layers.append(nn.Linear(input_size // (2**self.LAYERS), output_size))
+        #layers.append(nn.Linear(input_size // (2**self.LAYERS), output_size))
         self.seq = nn.Sequential(*layers)
 
     def forward(self, x):
